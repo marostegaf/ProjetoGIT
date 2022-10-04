f0b5996d7352deae2fdc1fea9f82099e2755992f
Marostega <103620713+marostegaf@users.noreply.github.com>
2022-10-04 14:30:56 -0300

    Delete main.py

--- Mundo 1/pythonProject/main.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite o nome: ")
-frase = "qj41uksaeod3r8aaorhs 9m4s2zsurs8atz9 nf2bb11uvç3n1zzd~dca"
-contagem = 0
-while contagem <= 10:
-    print("Juntando palavras e calculanddo...")
-    contagem += 1
-print(f"Do {nome} oq eu mais quero é: {frase[24::4].lower()}")
