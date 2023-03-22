02238c91fc3c960039ca57117aa6d2842d92691d
Marostega <103620713+marostegaf@users.noreply.github.com>
2023-03-21 23:20:54 -0300

    Delete JavaGIT directory

--- JavaGIT/Library/Ano.java
+++
@@ -1,9 +0,0 @@
-import java.util.Calendar;
-
-public class Ano {
-    public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        int ano = cal.get(Calendar.YEAR);
-        System.out.println(ano);
-    }
-}
